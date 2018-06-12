a9533db6352aea2d93bc5d79ec7152191e505323
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-06-12 15:48:03 -0400

    Initial setup/updates for pfswift.

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -24,12 +24,11 @@ str_desc = """
 """
 
 
-def pman_do(args, unknown):
+def pfswift_do(args, unknown):
 
     str_otherArgs   = ' '.join(unknown)
 
-    str_CMD = "/usr/local/bin/pman %s" % (str_otherArgs)
-    # str_CMD = "/usr/local/pman/bin/pman %s" % (str_otherArgs)
+    str_CMD = "/usr/local/bin/pfswift %s" % (str_otherArgs)
     return str_CMD
 
 parser  = ArgumentParser(description = str_desc, formatter_class = RawTextHelpFormatter)
@@ -46,7 +45,7 @@ args, unknown   = parser.parse_known_args()
 
 if __name__ == '__main__':
     try:
-        fname   = 'pman_do(args, unknown)'
+        fname   = 'pfswift_do(args, unknown)'
         str_cmd = eval(fname)
         # print(str_cmd)
         os.system(str_cmd)
